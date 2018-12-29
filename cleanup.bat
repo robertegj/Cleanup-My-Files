8df3412186d2c054afb089716ff6f98fa2074970
Robert G <robertegj@gmail.com>
2018-12-29 13:58:30 -0500

    Make cleanup.bat
    Sudo ./configure && make && make install

---
+++ cleanup.bat
@@ -0,0 +1,57 @@
+REM ---- We clean. 
+REM ----------------------------------------------------
+REM ---- Place this anywhere (on a windows system), ----
+REM ---- and run it to sort all adjacent files      ----
+REM ---- by type into their respective user folder. ----
+REM -- Cleans pictures, music, videos, and documents. --
+REM ----------------------------------------------------
+
+@echo off
+title Cleaning House!
+
+set urPictures=C:/Users/%username%/Pictures/
+set urMusic=C:/Users/%username%/Music/
+set urVideos=C:/Users/%username%/Videos/
+set urDocuments=C:/Users/%username%/Documents/
+
+echo moving pictures to %urPictures%
+move *.png %urPictures%
+move *.jpg %urPictures%
+move *.jpeg %urPictures%
+move *.gif %urPictures%
+move *.xcf %urPictures%
+
+echo moving music to %urMusic%
+move *.mp3 %urMusic%
+move *.wav %urMusic%
+move *.flac %urMusic%
+move *.ogg %urMusic%
+
+echo moving videos to %urVideos%
+move *.wmv %urVideos%
+move *.mp4 %urVideos%
+move *.flv %urVideos%
+move *.mov %urVideos%
+move *.mkv %urVideos%
+move *.avi %urVideos%
+
+echo moving documents to %urDocuments%
+move *.txt %urDocuments%
+move *.docx %urDocuments%
+move *.xls %urDocuments%
+move *.doc %urDocuments%
+move *.pdf %urDocuments%
+move *.epub %urDocuments%
+move *.torrent %urDocuments%
+move *.vcf %urDocuments%
+move *.py %urDocuments%
+move *.htm %urDocuments%
+move *.html %urDocuments%
+move *.php %urDocuments%
+move *.ovpn %urDocuments%
+
+title Cleaned!
+color 0a
+cls
+echo All clean! What do you think, %username%?
+pause
